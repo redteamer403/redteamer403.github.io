1bc9b380340a33155dd244ccf7a383cad77965d4
redteamer403 <rustamf1991@gmail.com>
2025-02-14 08:54:51 -0500

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/redteamer403.github.io/',  // Set this to your repo name
+  base: './',  // Set this to your repo name
   build: {
     outDir: 'docs'  // GitHub Pages expects files in 'docs' folder
   }
